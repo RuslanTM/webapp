712e56909505cc7812235566dc5fc2539690ccf3
RuslanTM <rustemirgali@gmail.com>
2015-05-13 21:21:20 +0600

    Using GetClass()

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,11 +1,16 @@
 package lesson2;
-
+import com.sun.org.apache.xpath.internal.SourceTree;
+import kz.javawebinar.webapp.model.*;
 /**
  * Aser
  * 13.05.2015
  */
 public class Main {
     public static void main(String[] args) {
+        Link l1 = new Link("JavaWebinar","javawebinar.kz");
+        Link l2 = new Link(l1);
+        System.out.println(l2.getClass());
+        System.out.println(l1);
 
     }
 }
